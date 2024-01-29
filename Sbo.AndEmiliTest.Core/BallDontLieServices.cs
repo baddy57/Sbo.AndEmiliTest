29a73f0fada481b3c424de6550e18d4910c33d82
Cristian Baldi <baddy227@gmail.com>
2024-01-30 00:52:00 +0100

    minor refactor

--- Sbo.AndEmiliTest.Core/BallDontLieServices.cs
+++ Sbo.AndEmiliTest.Core/BallDontLieServices.cs
@@ -12,9 +12,12 @@ public class BallDontLieServices
         this.httpClient = httpClient;
     }
 
+    /// <summary>
+    /// Retrieves the 100 top nba scorers for the current season
+    /// </summary>
     public async Task<IEnumerable<(Player Player, int Points)>> GetTopScorers(CancellationToken ct)
     {
-        List<(Player Player, int Points)> data = [];
+        List<(Player Player, int Points)> allStats = [];
 
         for (int page = 0, totalPages = int.MaxValue;
             page < totalPages && !ct.IsCancellationRequested;
@@ -29,22 +32,30 @@ public class BallDontLieServices
 
             totalPages = response.meta.total_pages;
 
-            foreach (var datum in response.data.Where(x => x.pts is not null))
-            {
-                data.Add((datum.player, datum.pts!.Value));
-            }
+            var stats = response.data
+                .Where(x => x.pts is not null)
+                .Select(datum => (datum.player, datum.pts!.Value));
+
+            allStats.AddRange(stats);
         }
 
-        var top100 = data.GroupBy(x => x.Player.id)
-            .Select(x =>
-            {
-                var player = x.First().Player;
-                var totalPoints = x.Sum(tuple => tuple.Points);
-                return (player, totalPoints);
-            })
+        var top100 = AggregatePoints(allStats)
             .OrderBy(x => x.totalPoints)
             .Take(100);
 
         return top100;
     }
+
+    /// <summary>
+    /// aggregates points by player id
+    /// </summary>
+    /// <returns>a list of unique players and their total points</returns>
+    private static IEnumerable<(Player player, int totalPoints)> AggregatePoints(List<(Player Player, int Points)> data)
+        => data.GroupBy(x => x.Player.id)
+            .Select(x =>
+            {
+                var player = x.First().Player;
+                var totalPoints = x.Sum(tuple => tuple.Points);
+                return (player, totalPoints);
+            });
 }
