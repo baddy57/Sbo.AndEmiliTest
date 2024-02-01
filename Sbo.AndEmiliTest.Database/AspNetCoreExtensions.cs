73441708a837b4d827ee5d76639f9c026106d942
Cristian Baldi <baddy227@gmail.com>
2024-02-01 14:36:59 +0100

    ADD DI ext

---
+++ Sbo.AndEmiliTest.Database/AspNetCoreExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Sbo.AndEmiliTest.Database;
+
+public static class AspNetCoreExtensions
+{
+    public static IServiceCollection AddAndEmiliTestDbContextAndFactory(this IServiceCollection services)
+    {
+        services.AddDbContextFactory<SboAndEmiliTestContext>();
+        services.AddDbContext<SboAndEmiliTestContext>();
+        return services;
+    }
+}
