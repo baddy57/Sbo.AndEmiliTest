db6530f918c51ce08ccf5369d0d49d9657110e87
Cristian Baldi <baddy227@gmail.com>
2024-02-01 15:05:51 +0100

    ADD InitializePlayersAndStats

--- Sbo.AndEmiliTest.Core/BallDontLieServices.cs
+++ Sbo.AndEmiliTest.Core/BallDontLieServices.cs
@@ -1,42 +1,121 @@
 ﻿using System.Text.Json;
-using Sbo.AndEmiliTest.Core.Dto;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Sbo.AndEmiliTest.Core.BallDontLieDtos;
+using Sbo.AndEmiliTest.Database;
 
 namespace Sbo.AndEmiliTest.Core;
 
 public class BallDontLieServices
 {
     private readonly HttpClient httpClient;
+    private ILogger<BallDontLieServices> logger;
+    private IDbContextFactory<SboAndEmiliTestContext> dbContextFactory;
 
-    public BallDontLieServices(HttpClient httpClient)
+    public BallDontLieServices(HttpClient httpClient, ILogger<BallDontLieServices> logger, IDbContextFactory<SboAndEmiliTestContext> dbContextFactory)
     {
         this.httpClient = httpClient;
+        this.logger = logger;
+        this.dbContextFactory = dbContextFactory;
+    }
+
+    /// <summary>
+    /// pulls current season stats and players from balldontlie and stores them in the local db
+    /// </summary>
+    public async Task InitializePlayersAndStats(CancellationToken ct)
+    {
+        //exclude latest data to avoid handling games being played
+        var maxDate = (DateTime.Now - TimeSpan.FromDays(2)).Date;
+
+        logger.LogInformation("Beginning data download...");
+
+        int page = 0;
+        while (!ct.IsCancellationRequested)
+        {
+            var httpResponse = await httpClient.GetAsync($"https://www.balldontlie.io/api/v1/stats?page={page}&per_page={100}&seasons[]=2023&postseason=false&end_date={maxDate:YYYY-MM-DD}", ct);
+            var json = await httpResponse.Content.ReadAsStringAsync(ct);
+
+            //todo better retry policy
+            //todo store fetched data and download only newer stats
+            //todo sql view w/ aggregate points
+            if (json.Equals("Retry later\n"))
+            {
+                await Task.Delay(5000, ct);
+                logger.LogWarning("Api call limit reached {page}", page);
+                continue;
+            }
+
+            var response = JsonSerializer.Deserialize<Root>(json)
+                ?? throw new InvalidDataException("Could not parse data");
+            if (response.data.Count == 0)
+                break;
+
+            logger.LogInformation("Downloaded {count} records", response.data.Count);
+
+            var stats = response.data
+                .Where(x => x.pts is not null);
+
+            using (var context = await dbContextFactory.CreateDbContextAsync(ct))
+            {
+                await context.NbaPlayers.AddRangeAsync(
+                    stats.Select(x => x.player.ToEntity())
+                        .DistinctBy(x => x.Id), ct);
+                await context.NbaPlayerStats.AddRangeAsync(
+                    stats.Select(x => x.ToEntity()), ct);
+                await context.SaveChangesAsync(ct);
+            }
+
+            if (response.meta.next_page is null)
+                break;
+
+            page++;
+        }
+        logger.LogInformation("Player stats downloaded");
     }
 
     /// <summary>
     /// Retrieves the 100 top nba scorers for the current season
     /// </summary>
+    [Obsolete]
     public async Task<IEnumerable<(Player Player, int Points)>> GetTopScorers(CancellationToken ct)
     {
         List<(Player Player, int Points)> allStats = [];
 
-        for (int page = 0, totalPages = int.MaxValue;
-            page < totalPages && !ct.IsCancellationRequested;
-            page++)
+        int page = 0;
+        while (!ct.IsCancellationRequested)
         {
-            var httpResponse = await httpClient.GetAsync($"https://www.balldontlie.io/api/v1/stats?page={page}&per_page=100", ct);
+            logger.LogTrace("Fetching page {page}", page);
+
+            var httpResponse = await httpClient.GetAsync($"https://www.balldontlie.io/api/v1/stats?page={page}&per_page={100}&seasons[]=2023&postseason=false", ct);
 
             var json = await httpResponse.Content.ReadAsStringAsync(ct);
 
-            var response = JsonSerializer.Deserialize<Root>(json)
-                ?? throw new InvalidDataException();
+            //todo better retry policy
+            //todo store fetched data and download only newer stats
+            //todo sql view w/ aggregate points
+            if (json.Equals("Retry later\n"))
+            {
+                await Task.Delay(5000);
+                logger.LogWarning("Api call limit reached {page}", page);
+                continue;
+            }
 
-            totalPages = response.meta.total_pages;
+            var response = JsonSerializer.Deserialize<Root>(json)
+                ?? throw new InvalidDataException("Could not parse data");
+            if (response.data.Count == 0)
+                break;
 
             var stats = response.data
                 .Where(x => x.pts is not null)
                 .Select(datum => (datum.player, datum.pts!.Value));
 
             allStats.AddRange(stats);
+
+            if (response.meta.next_page is null)
+                break;
+
+            page++;
+            //await Task.Delay(1000);
         }
 
         var top100 = AggregatePoints(allStats)
@@ -50,6 +129,7 @@ public class BallDontLieServices
     /// aggregates points by player id
     /// </summary>
     /// <returns>a list of unique players and their total points</returns>
+    [Obsolete]
     private static IEnumerable<(Player player, int totalPoints)> AggregatePoints(List<(Player Player, int Points)> data)
         => data.GroupBy(x => x.Player.id)
             .Select(x =>
--- Sbo.AndEmiliTest.Core/Mappers.cs
+++ Sbo.AndEmiliTest.Core/Mappers.cs
@@ -5,12 +5,19 @@ namespace Sbo.AndEmiliTest.Core;
 internal static class Mappers
 {
     internal static Database.NbaPlayerStat ToEntity(this StatsResponse stat)
-        => new Database.NbaPlayerStat()
+        => new()
         {
             Id = stat.id,
             Date = stat.game.date,
             GameId = stat.game.id,
             NbaPlayerId = stat.player.id,
             Points = stat.pts ?? 0
+        };
+
+    internal static Database.NbaPlayer ToEntity(this Player player)
+        => new()
+        {
+            Id = player.id,
+            Name = string.Join(' ', [player.first_name, player.last_name]),
         };
 }
