1495a25a9d272a2347b55f39a6892c61dc31c732
Cristian Baldi <baddy227@gmail.com>
2024-02-01 17:26:16 +0100

    FIX using minimal api

--- Sbo.AndEmiliTest.ApiServer/Program.cs
+++ Sbo.AndEmiliTest.ApiServer/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Sbo.AndEmiliTest.ApiServer;
 using Sbo.AndEmiliTest.Core;
 using Sbo.AndEmiliTest.Database;
@@ -23,12 +24,72 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("top100", async (SboAndEmiliTestContext dbContext) =>
+{
+	var top100 = await dbContext.Top100NbaScorers.ToListAsync();
+
+	if (top100 is null)
+		return null;
+
+	return top100;
+});
+
+app.MapPut("setfav/{email}/playerid", async (string email,
+											int playerid,
+											SboAndEmiliTestContext dbContext) =>
+{
+	var user = await dbContext.Users.SingleOrDefaultAsync(x => x.Email == email) ?? throw new InvalidOperationException();
+	var player = await dbContext.NbaPlayers.FindAsync(playerid) ?? throw new InvalidOperationException();
+
+	//todo unique (user-player)
+	dbContext.UserFavouritePlayers.Add(new()
+	{
+		Player = player,
+		User = user
+	});
+});
+
+app.MapPut("unsetfav/{email}/{playerid}", async (string email,
+											int playerid,
+											SboAndEmiliTestContext dbContext) =>
+{
+	var user = await dbContext.Users.SingleOrDefaultAsync(x => x.Email == email)
+		?? throw new InvalidOperationException();
+	var player = await dbContext.NbaPlayers.FindAsync(playerid)
+		?? throw new InvalidOperationException();
+
+	//todo unique (user-player)
+	var x = await dbContext.UserFavouritePlayers.FirstOrDefaultAsync(x => x.User.Email == email && x.PlayerId == playerid)
+		?? throw new InvalidOperationException();
+
+	dbContext.UserFavouritePlayers.Remove(x);
+});
+
+app.MapGet("getfavs/{email}", async (string email, SboAndEmiliTestContext dbContext) =>
+{
+	var user = await dbContext.Users.SingleOrDefaultAsync(x => x.Email == email);
+
+	var players = user?.UserFavouritePlayers.Select(x => x.Player)
+		?? throw new InvalidOperationException();
+
+	return players;
+});
+
+app.MapPut("login/{email}", async (string email, SboAndEmiliTestContext dbContext) =>
+{
+	if (!await dbContext.Users.AnyAsync(x => x.Email == email))
+	{
+		dbContext.Users.Add(new() { Email = email });
+		await dbContext.SaveChangesAsync();
+	}
+});
 
 app.Run();
