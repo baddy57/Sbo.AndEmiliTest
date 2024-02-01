503532ae47191c5421ba9b57c8c988432d0852b7
Cristian Baldi <baddy227@gmail.com>
2024-02-01 17:31:31 +0100

    ADD webapp [WIP]

---
+++ Sbo.AndEmiliTest.WebApp/Data/SessionService.cs
@@ -0,0 +1,37 @@
+﻿using Blazored.LocalStorage;
+
+namespace Sbo.AndEmiliTest.WebApp.Data;
+
+public class SessionService
+{
+    private readonly ILocalStorageService localStorageService;
+    private const string UserKey = "User";
+
+    public SessionService(ILocalStorageService localStorageService)
+    {
+        this.localStorageService = localStorageService;
+    }
+
+    public async Task<string?> GetLoggedUser()
+    {
+        try
+        {
+            var user = await localStorageService.GetItemAsync<string?>(UserKey);
+            return user;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public async Task Login(string user)
+    {
+        await localStorageService.SetItemAsync(UserKey, user);
+    }
+
+    public async Task Logout()
+    {
+        await localStorageService.SetItemAsync<string?>(UserKey, null);
+    }
+}
