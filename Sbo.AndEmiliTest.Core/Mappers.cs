655636bf0e75c7709de1aed22f40097561618829
Cristian Baldi <baddy227@gmail.com>
2024-02-01 14:48:33 +0100

    ADD mappers

---
+++ Sbo.AndEmiliTest.Core/Mappers.cs
@@ -0,0 +1,16 @@
+﻿using Sbo.AndEmiliTest.Core.BallDontLieDtos;
+
+namespace Sbo.AndEmiliTest.Core;
+
+internal static class Mappers
+{
+    internal static Database.NbaPlayerStat ToEntity(this StatsResponse stat)
+        => new Database.NbaPlayerStat()
+        {
+            Id = stat.id,
+            Date = stat.game.date,
+            GameId = stat.game.id,
+            NbaPlayerId = stat.player.id,
+            Points = stat.pts ?? 0
+        };
+}
