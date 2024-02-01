6a542cbeaf1b0e5ce8fb0746596fd5e7629f6e16
Cristian Baldi <baddy227@gmail.com>
2024-02-01 17:14:23 +0100

    ADD controllers

--- Sbo.AndEmiliTest.ApiServer/Controllers/PlayersController.cs
+++ Sbo.AndEmiliTest.ApiServer/Controllers/PlayersController.cs
@@ -8,23 +8,64 @@ namespace Sbo.AndEmiliTest.ApiServer.Controllers;
 [Route("players")]
 public class PlayersController : ControllerBase
 {
-    private readonly SboAndEmiliTestContext dbContext;
-    private readonly ILogger<PlayersController> _logger;
+	private readonly SboAndEmiliTestContext dbContext;
+	private readonly ILogger<PlayersController> _logger;
 
-    public PlayersController(ILogger<PlayersController> logger, SboAndEmiliTestContext dbContext)
-    {
-        _logger = logger;
-        this.dbContext = dbContext;
-    }
+	public PlayersController(ILogger<PlayersController> logger, SboAndEmiliTestContext dbContext)
+	{
+		_logger = logger;
+		this.dbContext = dbContext;
+	}
 
-    [HttpGet("top100", Name = "GetTop100")]
-    public async Task<ActionResult<IEnumerable<Top100NbaScorer>>> GetTop100()
-    {
-        var top100 = await dbContext.Top100NbaScorers.ToListAsync();
+	[HttpGet("top100", Name = "GetTop100")]
+	public async Task<ActionResult<IEnumerable<Top100NbaScorer>>> GetTop100()
+	{
+		var top100 = await dbContext.Top100NbaScorers.ToListAsync();
 
-        if (top100 is null)
-            return NotFound();
+		if (top100 is null)
+			return NotFound();
 
-        return Ok(top100);
-    }
+		return Ok(top100);
+	}
+
+	[HttpGet("setfav")]
+	public async Task<ActionResult> SetFavourite(string email, int playerid)
+	{
+		var user = await dbContext.Users.SingleOrDefaultAsync(x => x.Email == email);
+		if (user is null)
+			return BadRequest();
+
+		var player = await dbContext.NbaPlayers.FindAsync(playerid);
+		if (player is null)
+			return BadRequest();
+
+		//todo unique (user-player)
+		dbContext.UserFavouritePlayers.Add(new()
+		{
+			Player = player,
+			User = user
+		});
+
+		return Ok();
+	}
+
+	[HttpGet("unsetfav")]
+	public async Task<ActionResult> UnSetFavourite(string email, int playerid)
+	{
+		var user = await dbContext.Users.SingleOrDefaultAsync(x => x.Email == email);
+		if (user is null)
+			return BadRequest();
+
+		var player = await dbContext.NbaPlayers.FindAsync(playerid);
+		if (player is null)
+			return BadRequest();
+
+		var x = await dbContext.UserFavouritePlayers.FirstOrDefaultAsync(x => x.User.Email == email && x.PlayerId == playerid);
+		if (x is null)
+			return BadRequest();
+
+		dbContext.UserFavouritePlayers.Remove(x);
+
+		return Ok();
+	}
 }
